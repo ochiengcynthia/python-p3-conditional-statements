eaf8f7cdfb75efcdd24d3755f900a1232c70ce53
ochiengcynthia <126596182+ochiengcynthia@users.noreply.github.com>
2023-05-16 15:31:30 +0300

    completed assignment

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,68 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-    # your code here
+    if  username == "ADMIN" or username == "admin" and password== "12345":
+        return "Access granted"
+    else:
+        return "Access denied"
     pass
 
 def hows_the_weather(temperature):
-    # your code here
+    if temperature < 40:
+        return "It's brisk out there!"
+    elif temperature >=40 and temperature <=65:
+            return "It's a little chilly out there!"
+    elif temperature > 85:
+                return "It's too dang hot out there!"
+    else:
+                    return "It's perfect out there!"
     pass
 
 def fizzbuzz(num):
-    # your code here
+    if num % 3 == 0 and num % 5 == 0:
+            return "FizzBuzz"
+    elif num % 3 == 0:
+            return "Fizz"
+    elif num % 5 == 0:
+                return "Buzz"
+    else:
+                    return num
     pass
 
 def calculator(operation, num1, num2):
-    # your code here
-    pass
+     if operation == "+":
+             return num1 + num2
+     elif operation == "-":
+               return num1 - num2
+     elif operation == "*":
+                return num1 * num2
+     elif operation == "/":
+                  return num1/ num2
+     else:
+              print ("Invalid operation!")
+              return None
+    
+
+
+
+# def divide(num1, num2):
+#     try:
+#         quotient = num1 / num2
+#         print(quotient)
+#     except ZeroDivisionError:
+#         print("Error: num2 cannot be equal to 0")
+#     except TypeError:
+#         print("Error: input must be of type int or float")
+    
+# divide(5,10)
+# def divide(num1, num2):
+#     try:
+#         quotient = num1 / num2
+#         print(quotient)
+#     except ZeroDivisionError:
+#         print("Error: num2 cannot be equal to 0")
+#     except TypeError:
+#         print("Error: input must be of type int or float")
+#     finally:
+#         print("Isn't division fun?")
+# divide(5,10)
